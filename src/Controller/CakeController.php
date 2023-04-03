16569d0cad3c4eb22f90c7915c3f495884013204
Mehdi2707 <mehdi2707@live.fr>
2023-04-03 13:53:23 +0200

    dev template cake zone

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -13,7 +13,48 @@ class CakeController extends AbstractController
     public function index(): Response
     {
         return $this->render('cake/index.html.twig', [
-            'controller_name' => 'CakeController',
+        ]);
+    }
+
+    #[Route('/apropos', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('cake/about.html.twig', [
+        ]);
+    }
+
+    #[Route('/menu', name: 'menu')]
+    public function menu(): Response
+    {
+        return $this->render('cake/menu.html.twig', [
+        ]);
+    }
+
+    #[Route('/equipe', name: 'team')]
+    public function team(): Response
+    {
+        return $this->render('cake/team.html.twig', [
+        ]);
+    }
+
+    #[Route('/services', name: 'services')]
+    public function services(): Response
+    {
+        return $this->render('cake/services.html.twig', [
+        ]);
+    }
+
+    #[Route('/temoignages', name: 'testimonial')]
+    public function testimonial(): Response
+    {
+        return $this->render('cake/testimonial.html.twig', [
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('cake/contact.html.twig', [
         ]);
     }
 }
